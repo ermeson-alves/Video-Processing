9e21b30597cf3f76555e53aeca7407fd190db988
Ermeson Alves <ermeson3alves@gmail.com>
2023-11-23 19:14:30 -0300

    Esta exibindo video corretamente no Windows

--- read_and_visualization.py
+++ read_and_visualization.py
@@ -7,20 +7,17 @@ import numpy as np
 video_path = './videos/part2(Ermeson).mp4'
 video = cv2.VideoCapture(video_path)
 
+ret = True
+
+while ret:
+
+    ret, frame = video.read() 
+    if ret == True: 
+    # Display the resulting frame 
+        cv2.imshow('Frame', frame) 
+          
+    # Press Q on keyboard to exit 
+        if cv2.waitKey(15) & 0xFF == ord('q'): 
+            break
 
 
-# while ret:
-#     ret, frame = video.read()
-
-#     if ret:
-#         cv2.namedWindow('Nome da Janela', cv2.WINDOW_NORMAL)
-#         cv2.imshow('Nome da Janela', frame)
-#         if cv2.waitKey(0) & 0xFF == ord('q'):
-#             break
-
-img = cv2.imread('videos/img.jpg')
-# ret, frame = video.read()
-# print("Ret:", ret)
-plt.imshow(img)
-
-plt.show()
