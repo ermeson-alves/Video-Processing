0ac5dc19cdcbddd6ba5174b6e953289ede4cf158
Ermeson Alves <ermeson3alves@gmail.com>
2023-12-12 18:05:05 -0300

    Segmentação baseada em canal Hue + Adição do codigo do Carlos

--- classic_segmentation.py
+++ classic_segmentation.py
@@ -1,60 +1,83 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from preprocessing import canny
+import time
 
 
 # path do video
-video_path = './videos/part2(Ermeson).mp4'
+video_path = 'videos\part2(Ermeson).mp4'
 video = cv2.VideoCapture(video_path)
 
 
-# while True:
-#     ret, frame = video.read() 
-#     if not ret:
-#         break 
-          
-#     # Show:
-#     cv2.imshow('Video', frame)
-#     # Press Q on keyboard to exit 
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-ret1, frame_1 = video.read()
-ret2, frame_2 = video.read()
 
-c_frame_1 = canny(frame_1)
-c_frame_2 = canny(frame_2)
-
-# plt.subplot(141); plt.imshow(frame_1-frame_2, cmap='gray'); plt.title('Frame 1 - Frame 2')
-# plt.subplot(142); plt.imshow(c_frame_1-frame_2, cmap='gray'); plt.title('CANNY(Frame 1) - Frame 2')
-# plt.subplot(143); plt.imshow(frame_1-c_frame_2, cmap='gray'); plt.title('Frame 1 - CANNY(Frame 2)')
-# plt.subplot(144); plt.imshow(c_frame_1-c_frame_2, cmap='gray'); plt.title('CANNY(Frame 1) - CANNY(Frame 2)')
-
-# plt.figure(figsize=(20, 10))
-# plt.subplot(141); plt.imshow(frame_1[..., 0], cmap='gray'); plt.title('Canal azul')
-# plt.subplot(142); plt.imshow(frame_1[..., 2], cmap='gray'); plt.title('Canal vermelho')
-# plt.subplot(143); plt.imshow(frame_1[..., 0] - frame_1[..., 2], cmap='gray'); plt.title('Azul - Vermelho')
+# ret1, frame_1 = video.read()
+# ret2, frame_2 = video.read()
 
 
-elementoEstruturante	=	cv2.getStructuringElement(
-		cv2.MORPH_ELLIPSE,	(5,5)
-)
-imagemProcessada	=	cv2.dilate (
-		frame_2- frame_1,	elementoEstruturante,	iterations	=	2
-)
+# kernel	=	cv2.getStructuringElement(
+# 		cv2.MORPH_ELLIPSE,	(5,5)
+# )
 
 
-b,g,r = cv2.split(frame_1)
-h, s, v = cv2.split(cv2.cvtColor(frame_1, cv2.COLOR_BGR2HSV))
 
-plt.subplot(231); plt.imshow(b, cmap='gray'); plt.title('B')
-plt.subplot(232); plt.imshow(g, cmap='gray'); plt.title('G')
-plt.subplot(233); plt.imshow(r, cmap='gray'); plt.title('R')
-plt.subplot(234); plt.imshow(h, cmap='gray'); plt.title('H')
-plt.subplot(235); plt.imshow(s, cmap='gray'); plt.title('S')
-plt.subplot(236); plt.imshow(v, cmap='gray'); plt.title('V')
+# b,g,r = cv2.split(frame_1)
+# h, s, v = cv2.split(cv2.cvtColor(frame_1, cv2.COLOR_BGR2HSV))
+# h2, s2, v2 = cv2.split(cv2.cvtColor(frame_2, cv2.COLOR_BGR2HSV))
 
-plt.show()
+# ret, h = cv2.threshold(h, 160, 255, cv2.THRESH_BINARY)
+# ret, h2 = cv2.threshold(h2, 160, 255, cv2.THRESH_BINARY)
+
+
+# h = cv2.dilate(h, kernel, iterations=2)
+# h2 = cv2.dilate(h2, kernel, iterations=2)
+
+
+# plt.subplot(331); plt.imshow(b, cmap='gray'); plt.title('B')
+# plt.subplot(332); plt.imshow(g, cmap='gray'); plt.title('G')
+# plt.subplot(333); plt.imshow(r, cmap='gray'); plt.title('R')
+# plt.subplot(334); plt.imshow(h, cmap='gray'); plt.title('H')
+# plt.subplot(335); plt.imshow(s, cmap='gray'); plt.title('S')
+# plt.subplot(336); plt.imshow(v, cmap='gray'); plt.title('V')
+# plt.subplot(337); plt.imshow(h, cmap='gray'); plt.title('H1')
+# plt.subplot(338); plt.imshow(h2, cmap='gray'); plt.title('H2')
+# plt.subplot(339); plt.imshow(h2 - h, cmap='gray'); plt.title('H2 - H1')
+
+# # plt.imshow(frame_2 - frame_1)
+# plt.show()
+
+
+cor_pixel = 255
+
+cont = 0
+while True:
+	# calcular a distancia q o navio se moveu a cada 1 frame. Compara o frame n+1 com o n
+	ret, frame = video.read()
+
+	if not ret:
+		print("Não foi possivel ler o frame. Saindo...")
+		break
+	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7))
+	h, s, v = cv2.split(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
+	ret, h = cv2.threshold(h, 160, 255, cv2.THRESH_BINARY)
+	h = cv2.dilate(h, kernel, iterations=2)
+
+
+	pixels_non_zero = cv2.findNonZero(h)
+	coord_y = np.max(pixels_non_zero, axis=0)[0,1]
+
+	ponto_inicial = (0, coord_y) 
+	ponto_final = (h.shape[1], coord_y)
+	cor_linha = 255
+	espessura = 2
+	cv2.line(h, ponto_inicial, ponto_final, cor_linha, espessura)
+
+	cv2.imshow("Video", h)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+
+
 video.release()
 cv2.destroyAllWindows()
 
---
+++ trackship.py
@@ -0,0 +1,116 @@
+import cv2
+import numpy as np
+
+def trackShip(img, column):
+
+    # funcao que procura um pixel do navio partindo do pier, contando pixels de baixo para cima numa coluna
+
+    # pixel acima do qual o algoritmo ira procurar o navio (abaixo deste pixel se encontra o pier)
+    counter = 520
+
+    diff = 0
+
+    # cada pixel tem seus valores de cor comparado com o pixel acima
+    # caso haja uma diferenca acima do threshhold, foi encontrado um pixel do navcio
+    while (diff < 280):
+
+        if img[counter, column, 0] > img[counter - 1, column, 0]:
+            bigger0 = img[counter, column, 0]
+            smaller0 = img[counter - 1, column, 0]
+        else:
+            bigger0 = img[counter - 1, column, 0]
+            smaller0 = img[counter, column, 0]
+
+        if img[counter, column, 1] > img[counter - 1, column, 1]:
+            bigger1 = img[counter, column, 1]
+            smaller1 = img[counter - 1, column, 1]
+        else:
+            bigger1 = img[counter - 1, column, 1]
+            smaller1 = img[counter, column, 1]
+
+        if img[counter, column, 2] > img[counter - 1, column, 2]:
+            bigger2 = img[counter, column, 2]
+            smaller2 = img[counter - 1, column, 2]
+        else:
+            bigger2 = img[counter - 1, column, 2]
+            smaller2 = img[counter, column, 2]
+
+        diff = (bigger0 - smaller0) + (bigger1 - smaller1) + (bigger2 - smaller2) * 2
+
+        counter -= 1
+
+        # caso o navio nao tenha sido encontrado, retorna 0
+        if counter == 0:
+            return 0
+
+    return counter
+
+
+# path do video
+video_path = 'videos/part3.mp4'
+cap = cv2.VideoCapture(video_path)
+
+# kernel para realcar a imagem, deixando a diferenca de pixels mais acentuada
+kernel = np.array([[0.0, -1.0, 0.0],
+                   [-1.0, 5.0, -1.0],
+                   [0.0, -1.0, 0.0]])
+
+kernel = kernel/(np.sum(kernel) if np.sum(kernel) != 0 else 1)
+
+ret = True
+ship_found = False
+
+frame_counter = 0
+frame_array = []
+
+# enquanto houverem frames do video, o loop ira continuar
+# o loop pode ser encerrado apertando a tecla "q" na janela do video
+while ret:
+    # ret = true se ha mais um frame no video
+    # frame = imagem do frame do video
+    ret, frame = cap.read()
+
+    if ret:
+
+        # print('TRACKING NEXT FRAME')
+
+        # nesse array serao armazenados os pixels das bordas do navio encontradas
+        borders = []
+
+        # aplicando o filtro de realce
+        img = cv2.filter2D(frame, -1, kernel)
+
+        # 20 colunas de pixels sao analisadas
+        for column in range(500, 861, 18):
+            counter = trackShip(img, column)
+            borders.append(counter)
+
+        # ordena o array de pixels da borda do navio e leva em consideracao os 7 pixels centrais para encontrar a media
+        # esta operacao serve para retirar os pixels outliers e estabilizar o tracking do navio
+        borders.sort()
+        location = np.mean(borders[6:14])
+
+        # converte a medida de pixels para metros
+        position = (520-location)*0.32
+
+        # valores de posicao dos ultimos 10 segundos sao levados em consideracao para calculo da velocidade
+        if frame_counter < 240:
+            frame_counter += 1
+            frame_array.append(position)
+        else:
+            # velocidade do navio (metros por segundo)
+            speed = abs(np.mean(frame_array[:119])-np.mean(frame_array[120:]))/5
+            # posicao do navio (distancia em metros do pier ao navio)
+            present_position = np.mean(frame_array[120:])
+            print(present_position)
+            print(speed)
+            frame_array.pop(0)
+            frame_array.append(position)
+
+        # insere na imagem do video uma linha horizontal vermelha indicando como esta sendo feito o tracking
+        frame[(round(location) - 1):(round(location) + 1), (680-180):(680+180)] = [0, 0, 255]
+
+        # mostra a imagem do frame na janela de video
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
